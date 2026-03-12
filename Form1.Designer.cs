77e1ba1744c0655a6610f4d997099e2ea3a4dcfe
원준 최 <wjchoy2006@naver.com>
2026-03-12 17:09:23 +0900

    1단계완

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(283, 145);
+            button1.Name = "button1";
+            button1.Size = new Size(167, 63);
+            button1.TabIndex = 0;
+            button1.Text = "나를 잡아봐";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.MouseEnter += button1_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,37 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            int maxX = this.ClientSize.Width - button1.Width;
+            int maxY = this.ClientSize.Height - button1.Height;
+
+
+            int nextX = rand.Next(0, maxX);
+            int nextY = rand.Next(0, maxY);
+
+
+            button1.Location = new Point(nextX, nextY);
+
+
+            this.Text = $"버튼 좌표 - X: {nextX}, Y: {nextY}";
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+           
+            int maxX = this.ClientSize.Width - button1.Width;
+            int maxY = this.ClientSize.Height - button1.Height;
+
+            
+            button1.Location = new Point(rand.Next(0, maxX), rand.Next(0, maxY));
+
+            this.Text = $"X: {button1.Location.X}, Y: {button1.Location.Y}";
+        }
     }
 }
